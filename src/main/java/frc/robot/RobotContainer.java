331618355e87902976070630bfafdb81d851d528
Ender717 <nathan.sandvig@gmail.com>
2024-02-06 19:22:35 -0600

    new pathplanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,7 @@ import frc.robot.Commands.ZeroHeading;
 import frc.robot.Constants.ButtonConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 
 /*
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -28,89 +29,101 @@ import frc.robot.subsystems.DriveSubsystem;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems
-    private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+        // The robot's subsystems
+        private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+        private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
 
-    private final SendableChooser<Command> autoChooser;
+        private final SendableChooser<Command> autoChooser;
 
-    // The driver's controller
-    XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+        // The driver's controller
+        XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        // Configure the button bindings
-        configureButtonBindings();
+        /**
+         * The container for the robot. Contains subsystems, OI devices, and commands.
+         */
+        public RobotContainer() {
+                // Configure the button bindings
+                configureButtonBindings();
 
-        autoChooser = AutoBuilder.buildAutoChooser("Straight");
-        SmartDashboard.putData("Auto Chooser", autoChooser);
+                autoChooser = AutoBuilder.buildAutoChooser("Straight");
+                SmartDashboard.putData("Auto Chooser", autoChooser);
 
+                // m_robotDrive.resetEncoders();
 
-        // m_robotDrive.resetEncoders();
+                // Configure default commands
+                m_robotDrive.setDefaultCommand(
+                                // The left stick controls translation of the robot.
+                                // Turning is controlled by the X axis of the right stick.
+                                new RunCommand(
+                                                () -> m_robotDrive.drive(
+                                                                -MathUtil.applyDeadband(m_driverController.getLeftY(),
+                                                                                OIConstants.kDriveDeadband),
+                                                                -MathUtil.applyDeadband(m_driverController.getLeftX(),
+                                                                                OIConstants.kDriveDeadband),
+                                                                -MathUtil.applyDeadband(m_driverController.getRightX(),
+                                                                                OIConstants.kDriveDeadband),
+                                                                true, true),
+                                                m_robotDrive));
 
-        // Configure default commands
-        m_robotDrive.setDefaultCommand(
-                // The left stick controls translation of the robot.
-                // Turning is controlled by the X axis of the right stick.
-                new RunCommand(
-                        () -> m_robotDrive.drive(
-                                -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
-                                -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-                                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
-                                true, true),
-                        m_robotDrive));
-    }
+                m_IntakeSubsystem.setDefaultCommand(
+                                new RunCommand(() -> m_IntakeSubsystem.moveRoller(
+                                                -MathUtil.applyDeadband(m_driverController.getLeftTriggerAxis(),
+                                                                OIConstants.kDriveDeadband)),
+                                                m_IntakeSubsystem));
+        }
 
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
-     * subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
-     * passing it to a
-     * {@link JoystickButton}.
-     */
-    private void configureButtonBindings() {
-        new JoystickButton(m_driverController, Button.kR1.value)
-                .whileTrue(new RunCommand(
-                        () -> m_robotDrive.setX(),
-                        m_robotDrive));
+        /**
+         * Use this method to define your button->command mappings. Buttons can be
+         * created by
+         * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+         * subclasses ({@link
+         * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+         * passing it to a
+         * {@link JoystickButton}.
+         */
+        private void configureButtonBindings() {
+                new JoystickButton(m_driverController, Button.kR1.value)
+                                .whileTrue(new RunCommand(
+                                                () -> m_robotDrive.setX(),
+                                                m_robotDrive));
 
-        new JoystickButton(m_driverController, ButtonConstants.ButtonSelect)       //select button
-                .onTrue(new ZeroHeading(m_robotDrive));
+                new JoystickButton(m_driverController, ButtonConstants.ButtonSelect) // select button
+                                .onTrue(new ZeroHeading(m_robotDrive));
 
-        new JoystickButton(m_driverController, ButtonConstants.LeftBumper)
-                .whileTrue(new AimAndShoot(m_robotDrive));
+                new JoystickButton(m_driverController, ButtonConstants.LeftBumper)
+                                .whileTrue(new AimAndShoot(m_robotDrive));
 
+        }
 
-    }
+        /**
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         *
+         * @return the command to run in autonomous
+         */
+        public Command getAutonomousCommand() {
+                Field2d m_Field2d = new Field2d();
+                SmartDashboard.putData(m_Field2d);
+                
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        Field2d m_Field2d = new Field2d();
-        SmartDashboard.putData(m_Field2d);
+                /*
+                 * List<PathPlannerTrajectory> autoPaths1 =
+                 * PathPlanner.loadPathGroup(autoChooser.getSelected(),
+                 * Constants.AutoConstants.kMaxSpeedMetersPerSecond,
+                 * Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+                 * 
+                 * Command autoTest = new SequentialCommandGroup(
+                 * new FollowPathWithEvents(
+                 * new FollowPath(autoPaths1.get(0), m_robotDrive),
+                 * autoPaths1.get(0).getMarkers(),
+                 * Constants.AutoConstants.AUTO_EVENT_MAP)
+                 * );
+                 * 
+                 * 
+                 * m_Field2d.getObject("traj").setTrajectory(autoPaths1.get(0));
+                 * 
+                 * return autoTest;
+                 */
 
-        /* List<PathPlannerTrajectory> autoPaths1 = PathPlanner.loadPathGroup(autoChooser.getSelected(),
-                Constants.AutoConstants.kMaxSpeedMetersPerSecond,
-                Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared);
-
-        Command autoTest = new SequentialCommandGroup(
-                new FollowPathWithEvents(
-                        new FollowPath(autoPaths1.get(0), m_robotDrive),
-                        autoPaths1.get(0).getMarkers(),
-                        Constants.AutoConstants.AUTO_EVENT_MAP)
-        );
-
-
-        m_Field2d.getObject("traj").setTrajectory(autoPaths1.get(0));
-
-        return autoTest; */
-
-        return autoChooser.getSelected();
-    }
+                return autoChooser.getSelected();
+        }
 }
---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.*;
+import edu.wpi.first.math.controller.PIDController;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.math.MathUtil;
+
+
+public class IntakeSubsystem extends SubsystemBase {
+  // Initialization of variables
+  CANSparkMax rollersMotor;
+  CANSparkMax rollersMotor2;
+  PIDController pid;
+  double setDistance;
+  double holdPosition;
+
+  /** 
+   * Creates a new IntakeRollersSubsystem.
+   * Controls speed of intake rollers 
+  */
+  public IntakeSubsystem() {
+    // Creates a roller for intake
+    rollersMotor = new CANSparkMax(10, MotorType.kBrushless);
+    rollersMotor2 = new CANSparkMax(11, MotorType.kBrushless);
+
+    // Resets to default, always do before changing config
+    rollersMotor.restoreFactoryDefaults();
+    rollersMotor2.restoreFactoryDefaults();
+
+    rollersMotor.setClosedLoopRampRate(0.125);
+    rollersMotor2.setClosedLoopRampRate(0.125);
+  }
+
+  /**
+   * Moves the roller based on an axis, in this case, triggers
+   * @param speed double Input from triggers / axis
+   */
+  public void moveRoller(double speed){
+
+    // Set speed and reset encoder
+    // If not moving, try to get back to what it was when it stopped moving
+      rollersMotor.set(speed);
+      rollersMotor2.set(-speed);
+  }
+
+  /**
+   * Resets the encoder in the motor
+   */
+  public void reset(){
+    rollersMotor.getEncoder().setPosition(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
